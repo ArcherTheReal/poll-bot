b7f47995dcb757953d3168eb2b13bc408c1560dd
Archer <62026087+ArcherTheReal@users.noreply.github.com>
2024-02-07 21:46:17 +0100

    bot done

--- src/app.js
+++ src/app.js
@@ -2,9 +2,50 @@ const discord = require("discord.js");
 require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
+const mongoose = require("mongoose");
+const { type } = require("os");
+const Schema = mongoose.Schema;
+
+//poll schema
+const pollSchema = new Schema(
+  {
+    user: {
+      type: String,
+      required: true,
+    },
+    question: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    emojis: {
+      type: Array,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const settingsSchema = new Schema({
+  pollnumber: { type: Number, required: true },
+});
+
+const Poll = mongoose.model("Poll", pollSchema);
+const Settings = mongoose.model("Settings", settingsSchema);
+
+//mongodb
+mongoose
+  .connect(process.env.MONGODB)
+  .then((result) => console.log("Connected to db"))
+  .catch((err) => console.log(err));
 
 //settings
-const settings = fs.readFileSync(path.join(__dirname, "settings.json"));
+const settings = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "settings.json"))
+);
 
 //intents
 const intents = new discord.IntentsBitField([
@@ -23,6 +64,122 @@ client.on("ready", () => {
 });
 
 //poll slash command
-client.on("interactionCreate", async (interaction) => {});
+client.on("interactionCreate", async (interaction) => {
+  if (interaction.isButton()) {
+    if (interaction.customId === "copy-poll") {
+      const pollId = interaction.message.embeds[0].footer.text;
+      interaction.reply({ content: pollId, ephemeral: true });
+    }
+  }
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "poll") {
+    // Accessing the options
+    const question = interaction.options.getString("question");
+    let optionsArray = [];
+    let emojisArray = [];
+    for (let i = 1; i <= 10; i++) {
+      if (interaction.options.getString("option-" + i)) {
+        optionsArray.push(interaction.options.getString("option-" + i));
+        emojisArray.push(interaction.options.getString("emoji-" + i));
+      }
+    }
+
+    const poll = new Poll({
+      user: interaction.user.id,
+      question: question,
+      options: optionsArray,
+      emojis: emojisArray,
+    });
+    let fields = [];
+    for (let i = 0; i < optionsArray.length; i++) {
+      fields.push(emojisArray[i] + " " + optionsArray[i]);
+    }
+    poll
+      .save()
+      .then((result) => {
+        interaction.reply("Poll created!", { ephemeral: true });
+        const embed = new discord.EmbedBuilder()
+          .setColor("#0099ff")
+          .setTitle(question)
+          .setAuthor({
+            name: interaction.user.globalName,
+            iconURL: interaction.user.avatarURL(),
+          })
+          .setFooter({ text: result._id.toString() })
+          .setDescription(fields.join("\n"));
+
+        //button
+        const copy = new discord.ButtonBuilder()
+          .setCustomId("copy-poll")
+          .setLabel("Copy poll")
+          .setStyle(1);
+
+        const row = new discord.ActionRowBuilder().addComponents(copy);
+        interaction.channel.send({ embeds: [embed], components: [row] });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else if (interaction.commandName == "do-poll") {
+    if (!settings.admins.includes(interaction.user.id)) {
+      interaction.reply({ content: "You are not an admin", ephemeral: true });
+      return;
+    }
+
+    const pollId = interaction.options.getString("poll-id");
+    Settings.findById("65c3e1c42d6c743c589bbc22").then((pollnumber) => {
+      Poll.findById(pollId)
+        .then((result) => {
+          if (result.user) {
+            client.users.fetch(result.user).then(async (user) => {
+              let optionsArray = [];
+              for (let i = 0; i < result.options.length; i++) {
+                optionsArray.push(result.emojis[i] + " " + result.options[i]);
+              }
+              const embed = new discord.EmbedBuilder()
+                .setColor("#0099ff")
+                .setTitle("Random poll #" + pollnumber.pollnumber)
+                .setAuthor({
+                  name: user.globalName,
+                  iconURL: user.avatarURL(),
+                })
+                .setDescription(optionsArray.join("\n"))
+                .addFields({
+                  name: "**If you have any random poll ideas, submit them here!**",
+                  value: "<#1200118137367048282>",
+                });
+              await interaction.reply({ content: "Done", ephemeral: true });
+              const msg = await interaction.channel.send({ embeds: [embed] });
+              for (let i = 0; i < result.options.length; i++) {
+                msg.react(result.emojis[i]);
+              }
+              pollnumber.pollnumber++;
+              pollnumber.save();
+            });
+          } else {
+            interaction.reply({ content: "Poll not found", ephemeral: true });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          interaction.reply({ content: "An error occured", ephemeral: true });
+        });
+    });
+  } else if (interaction.commandName == "set-poll-number") {
+    if (!settings.admins.includes(interaction.user.id)) {
+      interaction.reply({ content: "You are not an admin", ephemeral: true });
+      return;
+    }
+    const number = interaction.options.getInteger("number");
+    Settings.findById("65c3e1c42d6c743c589bbc22").then((pollnumber) => {
+      pollnumber.pollnumber = number;
+      pollnumber.save();
+      interaction.reply({ content: "Done", ephemeral: true });
+    });
+  }
+});
 
 client.login(process.env.TOKEN);
--- src/register-commands.js
+++ src/register-commands.js
@@ -1,35 +1,163 @@
 require("dotenv").config();
 const { REST, Routes, ApplicationCommandOptionType } = require("discord.js");
 const fs = require("fs");
-const settings = fs.readFileSync("./settings.json");
+const { type } = require("os");
+const settings = JSON.parse(fs.readFileSync("src/settings.json", "utf8"));
 
 const commands = [
   {
     name: "poll",
-    description: "Start a poll",
+    description: "Suggest a poll",
     options: [
       {
-        name: "title",
-        description: "Title of the poll",
-        required: true,
-        type: String,
-      },
-      {
-        name: "origin",
-        description: "Who sent this question",
-        required: true,
-        type: ApplicationCommandOptionType.Mentionable,
-      },
-      {
         name: "question",
+        type: 3,
         description: "What is the question",
         required: true,
-        type: String,
       },
       {
-        name: "options",
-        description:
-          "first char should be emoji, options should be separated by | or ,",
+        name: "emoji-1",
+        type: 3,
+        description: "emoji-1",
+        required: true,
+      },
+      {
+        name: "option-1",
+        type: 3,
+        description: "option-1",
+        required: true,
+      },
+      {
+        name: "emoji-2",
+        type: 3,
+        description: "emoji-2",
+        required: false,
+      },
+      {
+        name: "option-2",
+        type: 3,
+        description: "option-2",
+        required: false,
+      },
+      {
+        name: "emoji-3",
+        type: 3,
+        description: "emoji-3",
+        required: false,
+      },
+      {
+        name: "option-3",
+        type: 3,
+        description: "option-3",
+        required: false,
+      },
+      {
+        name: "emoji-4",
+        type: 3,
+        description: "emoji-4",
+        required: false,
+      },
+      {
+        name: "option-4",
+        type: 3,
+        description: "option-4",
+        required: false,
+      },
+      {
+        name: "emoji-5",
+        type: 3,
+        description: "emoji-5",
+        required: false,
+      },
+      {
+        name: "option-5",
+        type: 3,
+        description: "option-5",
+        required: false,
+      },
+      {
+        name: "emoji-6",
+        type: 3,
+        description: "emoji-6",
+        required: false,
+      },
+      {
+        name: "option-6",
+        type: 3,
+        description: "option-6",
+        required: false,
+      },
+      {
+        name: "emoji-7",
+        type: 3,
+        description: "emoji-7",
+        required: false,
+      },
+      {
+        name: "option-7",
+        type: 3,
+        description: "option-7",
+        required: false,
+      },
+      {
+        name: "emoji-8",
+        type: 3,
+        description: "emoji-8",
+        required: false,
+      },
+      {
+        name: "option-8",
+        type: 3,
+        description: "option-8",
+        required: false,
+      },
+      {
+        name: "emoji-9",
+        type: 3,
+        description: "emoji-9",
+        required: false,
+      },
+      {
+        name: "option-9",
+        type: 3,
+        description: "option-9",
+        required: false,
+      },
+      {
+        name: "emoji-10",
+        type: 3,
+        description: "emoji-10",
+        required: false,
+      },
+      {
+        name: "option-10",
+        type: 3,
+        description: "option-10",
+        required: false,
+      },
+    ],
+  },
+  {
+    name: "do-poll",
+    description: "Admin only",
+    options: [
+      {
+        name: "poll-id",
+        type: 3,
+        description: "ID of poll",
+        required: true,
+      },
+    ],
+  },
+  {
+    name: "set-poll-number",
+    description: "Admin only",
+    options: [
+      {
+        name: "number",
+        type: 4,
+        description: "Number of polls",
+        required: true,
       },
     ],
   },
@@ -40,10 +168,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
   try {
     console.log("Registering slash commands...");
     await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
+      Routes.applicationGuildCommands(settings.clientId, settings.guildId),
       {
         body: commands,
       }
